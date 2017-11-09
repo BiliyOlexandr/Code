35348366b271da6fa6c488af284ab2e9237035a0
AlexZaitsev <zaytsevalexa@gmail.com>
2017-11-09 09:59:24 +0200

    Add link to AAC site (in TaskDB.class)

--- app/src/main/java/com/example/todolist/model/TaskDB.java
+++ app/src/main/java/com/example/todolist/model/TaskDB.java
@@ -4,6 +4,9 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
+
+
+//https://habrahabr.ru/post/332562/
 @Entity(tableName = "task")
 public class TaskDB {
   @PrimaryKey
